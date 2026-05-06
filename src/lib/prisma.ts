c174bb2dda1cce8c34ff93d5294a47d484484b8a
Gustavo Miranda <gustavosolucoes@yahoo.com>
2026-05-05 22:35:05 -0300

    fix: usa tipo Product do Prisma para resolver erro de build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,13 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-const databaseUrl = process.env.DATABASE_URL ?? 'file:./dev.db';
-const sqliteAdapter = new PrismaBetterSqlite3({ url: databaseUrl });
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ['query'],
-    adapter: sqliteAdapter,
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
