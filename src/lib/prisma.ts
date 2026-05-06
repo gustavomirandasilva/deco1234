d2ac09a40bd1656faf66ee742ef5d2b64ac2e302
Gustavo Miranda <gustavosolucoes@yahoo.com>
2026-05-06 17:27:01 -0300

    fix: usa tipo Product do Prisma para resolver erro de build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,13 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query"],
+    // Isso força o uso da engine de biblioteca em vez da engine de borda
+    // @ts-ignore
+    __internal: { engineType: 'library' } 
   })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
